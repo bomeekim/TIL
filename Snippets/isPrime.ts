64df63e135087c99e2fdb99e015e6e32eeb692b9
bomeekim <thespringg@naver.com>
2024-02-13 16:29:43 +0900

    [snippets] 소수 판별 로직 추가

---
+++ Snippets/isPrime.ts
@@ -0,0 +1,12 @@
+/**
+ * 소수 판별
+ */
+const isPrime = (n) => {
+    if (n === 1) return false
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false
+    }
+
+    return true
+}
